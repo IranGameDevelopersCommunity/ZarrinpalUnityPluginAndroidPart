dbc3c1473f2de72eaab46e85806f940bb8309eec
Mojtaba Pirveisi <mujpir@gmail.com>
2019-09-02 11:30:26 +0430

    Add purchase start callback fix a few bugs

--- unityzarinpaliab/src/main/java/com/kingcodestudio/unityzarinpaliab/ZarinpalActivity.java
+++ unityzarinpaliab/src/main/java/com/kingcodestudio/unityzarinpaliab/ZarinpalActivity.java
@@ -2,7 +2,6 @@ package com.kingcodestudio.unityzarinpaliab;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
@@ -13,12 +12,10 @@ import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import com.unity3d.player.UnityPlayer;
 import com.zarinpal.ewallets.purchase.OnCallbackRequestPaymentListener;
 import com.zarinpal.ewallets.purchase.OnCallbackVerificationPaymentListener;
@@ -33,6 +30,7 @@ public class ZarinpalActivity extends Activity {
     private static String m_merchantID;
     private static String m_callbackScheme;
     public static boolean m_verifyPurchase;
+    private static boolean m_autoStartPurchase;
 
     //Purchase parameters
     private static long m_amount;
@@ -40,6 +38,8 @@ public class ZarinpalActivity extends Activity {
     private static String m_title;
 
     ProgressDialog progress ;
+    private static Intent m_purchaseIntent;
+    private static Uri m_purchase_data;
 
     private void showProgress(){
         Resources res = getResources();
@@ -63,7 +63,9 @@ public class ZarinpalActivity extends Activity {
     public boolean onTouchEvent(MotionEvent event) {
         // If we've received a touch notification that the user has touched
         // outside the app, finish the activity.
-        if (MotionEvent.ACTION_OUTSIDE == event.getAction()) {
+        int actionType =  event.getAction();
+        Log.d("Zarinpal","onTouchEvent , actionType : "+actionType);
+        if (MotionEvent.ACTION_OUTSIDE == actionType) {
             cancelPurchase();
             return true;
         }
@@ -96,8 +98,8 @@ public class ZarinpalActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH, WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH);
-        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH, WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL);
 
         //Returning from payment gateway
         String action = getIntent().getAction();
@@ -107,6 +109,7 @@ public class ZarinpalActivity extends Activity {
             setContentView(R.layout.activity_verifying);
 
             Uri data = getIntent().getData();
+            m_purchase_data = data;
             if(data==null)
             {
                 Log.d("Zarinpal","zarinpal purchase returned null");
@@ -123,7 +126,8 @@ public class ZarinpalActivity extends Activity {
                     UnityPlayer.UnitySendMessage(UNITY_GAME_OBJECT_NAME,"OnPurchaseSucceed",authority);
                     if(m_verifyPurchase){
                         Log.d("Zarinpal","Starting to verify purchase cause autoVerify is set to true : ");
-                        verifyPurchase(data);
+                        verifyPurchase();
+                        finish();
                     }
                     else
                     {
@@ -174,14 +178,16 @@ public class ZarinpalActivity extends Activity {
         //ResizeActivity();
     }
 
-    public static void initialize(String merchantID,boolean verifyPurchase,String callbackScheme){
+    public static void initialize(String merchantID,boolean verifyPurchase,String callbackScheme,boolean autoStartPurchase){
         m_merchantID = merchantID; // Store merchant id
         m_verifyPurchase = verifyPurchase;//Should verify purchase after purchase completed
         m_callbackScheme = callbackScheme;//App Scheme used for zarinpal callback
+        m_autoStartPurchase = autoStartPurchase;
         UnityPlayer.UnitySendMessage(UNITY_GAME_OBJECT_NAME,"OnStoreInitialized","store initialized");
     }
 
     private void cancelPurchase(){
+        Log.d("Zarinpal","Purchase canceled by user");
         UnityPlayer.UnitySendMessage(UNITY_GAME_OBJECT_NAME,"OnPurchaseCanceled","cancel by user");
         finish();
     }
@@ -199,6 +205,7 @@ public class ZarinpalActivity extends Activity {
 
 
     private void purchase(long amount, String description){
+        m_purchaseIntent = null;
         EditText emailText = findViewById(R.id.emailText);
         EditText mobileText = findViewById(R.id.mobileText);
         ZarinPal purchase = ZarinPal.getPurchase(UnityPlayer.currentActivity);
@@ -221,8 +228,11 @@ public class ZarinpalActivity extends Activity {
             public void onCallbackResultPaymentRequest(int status, String authority, Uri paymentGatewayUri, Intent intent) {
                 if(status==100){
                     Log.d("Zarinpal","Payment started");
-                    UnityPlayer.UnitySendMessage(UNITY_GAME_OBJECT_NAME,"OnPurchaseStarted","null");
-                    UnityPlayer.currentActivity.startActivity(intent);
+                    UnityPlayer.UnitySendMessage(UNITY_GAME_OBJECT_NAME,"OnPurchaseStarted",authority);
+                    if(m_autoStartPurchase){
+                        UnityPlayer.currentActivity.startActivity(intent);
+                    }
+                    m_purchaseIntent = intent;
                 }
                 else
                 {
@@ -231,35 +241,72 @@ public class ZarinpalActivity extends Activity {
                 }
                 dismissProgress();
                 finish();
+
             }
         });
     }
 
-    private void verifyPurchase(Uri data){
-        Log.d("Zarinpal","Verifying purchase for : "+data.toString());
-        UnityPlayer.UnitySendMessage(UNITY_GAME_OBJECT_NAME,"OnPaymentVerificationStarted",data.toString());
-        ZarinPal.getPurchase(UnityPlayer.currentActivity).verificationPayment(data, new OnCallbackVerificationPaymentListener() {
-            @Override
-            public void onCallbackResultVerificationPayment(boolean isPaymentSuccess, String refID, PaymentRequest paymentRequest)
-            {
+    public static void startPurchaseActivity(){
+        if(m_purchaseIntent!=null){
+            UnityPlayer.currentActivity.startActivity(m_purchaseIntent);
+        }
+        else
+        {
+            UnityPlayer.UnitySendMessage(UNITY_GAME_OBJECT_NAME,"OnPurchaseFailedToStart","m_purchaseIntent is null.make sure to call startPurchaseFlow first");
+        }
+    }
 
-                if(isPaymentSuccess)
-                {
-                    Log.d("Zarinpal","Payment verify success");
-                    UnityPlayer.UnitySendMessage(UNITY_GAME_OBJECT_NAME,"OnPaymentVerificationSucceed",refID);
-                    setupReturnView(true);
-                }
-                else {
-                    Log.d("Zarinpal","Payment verify failed");
-                    UnityPlayer.UnitySendMessage(UNITY_GAME_OBJECT_NAME,"OnPaymentVerificationFailed","purchase is not valid");
-                    setupReturnView(false);
+    public static void verifyPurchase(String authority,int amount){
+        m_purchase_data = Uri.parse("dummyscheme://dummyhost?Authority="+authority+"&Status=OK");
+        PaymentRequest payment = ZarinPal.getPaymentRequest();
+        payment.setMerchantID(m_merchantID);
+        payment.setAuthority(authority);
+        payment.setAmount(amount);
+        ZarinPal verify = ZarinPal.getPurchase(UnityPlayer.currentActivity);
+        verify.setPayment(payment);
+        Log.d("Zarinpal","purchase_data created : "+m_purchase_data.toString());
+        verifyPurchase();
+    }
 
-                }
+    public static void verifyPurchase(){
+        if(m_purchase_data!=null)
+        {
+            Log.d("Zarinpal","Request for verify purchase : "+m_purchase_data.toString());
+            UnityPlayer.UnitySendMessage(UNITY_GAME_OBJECT_NAME,"OnPaymentVerificationStarted",m_purchase_data.toString());
+            String status = m_purchase_data.getQueryParameter("Status");
+            if(status!=null && status.equals("OK")){
+                Log.d("Zarinpal","Status ok . try to verify : "+m_purchase_data.toString());
 
+                ZarinPal.getPurchase(UnityPlayer.currentActivity).verificationPayment(m_purchase_data, new OnCallbackVerificationPaymentListener() {
+                    @Override
+                    public void onCallbackResultVerificationPayment(boolean isPaymentSuccess, String refID, PaymentRequest paymentRequest)
+                    {
 
-                finish();
+                        Log.d("Zarinpal","verification completed : "+m_purchase_data.toString());
+                        if(isPaymentSuccess)
+                        {
+                            Log.d("Zarinpal","Payment verify success");
+                            UnityPlayer.UnitySendMessage(UNITY_GAME_OBJECT_NAME,"OnPaymentVerificationSucceed",refID);
+                        }
+                        else {
+                            Log.d("Zarinpal","Payment verify failed");
+                            UnityPlayer.UnitySendMessage(UNITY_GAME_OBJECT_NAME,"OnPaymentVerificationFailed","purchase is not valid");
+
+                        }
+                    }
+                });
             }
-        });
+            else
+            {
+                Log.d("Zarinpal","status not ok . verify failed : "+m_purchase_data.toString());
+                UnityPlayer.UnitySendMessage(UNITY_GAME_OBJECT_NAME,"OnPaymentVerificationFailed","purchase status is NOK");
+            }
+        }
+        else
+        {
+            Log.d("Zarinpal","unable to verify purchase because data is null ");
+        }
+
     }
 
     private void setupReturnView(boolean success){
